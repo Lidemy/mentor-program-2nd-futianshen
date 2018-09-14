5e9e20a0e7de9197a967e923e44db1885f19175a
沈輔天 <futianshen@MacBook-Pro.local>
2018-09-14 21:16:34 +0800

    checkout

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,31 @@
-function alphaSwap(str) {
+//console.log(alphaSwap('nick'))
+//console.log(alphaSwap('Nick'))
+//console.log(alphaSwap(',HeLLo123'))
 
+ function alphaSwap(str) {
+	var counter = str.length-1 
+	var i = 0 // i 是 index 的意思
+	var newStr = ''
+	while(i<=counter) {
+		if (str[i]>='a' && str[i]<='z') newStr += str[i].toUpperCase()  //這種 if else 的用法我還不熟 和 if () {} 有什麼不同？
+		else if (str[i]>='A' && str[i]<='Z') newStr += str[i].toLowerCase()
+		else newStr += str[i]
+		i++
+	}
+	return newStr
 }
 
-module.exports = alphaSwap
+module.exports = alphaSwap 
+
+ /* 解題思路
+1 判斷第一個文字是大寫還小寫
+如果是大寫 回傳小寫
+如果是小寫 回傳大寫
+如果都不是 回傳原來的字母
+ 判斷第二個文字是大寫還小寫
+如果是大寫 回傳小寫
+如果是小寫 回傳大寫
+如果都不是 回傳原來的字母
+ 問題：要怎麽知道要判斷幾次文字？
+ */
+ //練習 用 ASCII 寫寫看
--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,33 @@
+//console.log(isPrime(2))
+//console.log(isPrime(3))
+//console.log(isPrime(10))
+//console.log(isPrime(37))
+//console.log(isPrime(38))
+
 function isPrime(n) {
   
+  var newArr =[]
+  var counter = 1
+  while (counter<=n) {
+  	if (n%counter===0) newArr.push(counter) 
+  	counter++
+  }
+  if (newArr.length===2) return true
+  else return false
 }
 
 module.exports = isPrime
+
+ /* 解題思路
+質數有什麼「獨有」的特徵？用特徵作為判斷的標準
+什麼是質數？
+因數只有兩個 一個是1 一個是自己
+除了 1 之外 其他小於自己的數都無法整除
+什麼是整除？不會有餘數
+ 判斷一個數是不是有幾個因數
+用迴圈
+ 如果=2個代表他是質數
+ 用 arr 寫寫看
+ ?運算子怎麼用？ ||
+*/	
+ //新題目 列出小於32的質數 
