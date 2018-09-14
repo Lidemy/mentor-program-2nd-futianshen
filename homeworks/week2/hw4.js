3e54f6643ce21f5ddf6fd97907d5c3c040093a0d
沈輔天 <futianshen@MacBook-Pro.local>
2018-09-14 15:45:06 +0800

    hw1 to hw5 complete

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,23 @@
+//console.log(isPalindromes('abcba'))
+//console.log(isPalindromes('apple'))
+
+
 function isPalindromes(str) {
-  
+	var newStr =''
+	var i = str.length-1
+	while (0<=i) {
+		newStr += str[i]
+		i--
+	}  
+	if (newStr===str) return true
+	else return false
 }
 
 module.exports = isPalindromes
+
+/* 
+1 先將字串翻過來
+迴圈
+2 判斷新字串和舊的字串相不相等
+===
+*/
